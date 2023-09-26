fdeac819d73900ba934df91b74fadd708d2ccb7d
mohamed-elnagar0 <mohamed304.m@gmail.com>
2023-09-26 17:14:42 +0300

    Generate Type Adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,18 @@
-class NoteModel {
+import 'package:hive/hive.dart';   //enable us to creat type adabter to store opject(mab)
+
+
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String data;
+  @HiveField(3)
   final int color;
 
   NoteModel({
