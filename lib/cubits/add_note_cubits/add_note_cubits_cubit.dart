cafb109128aa9116646be8e1991cc9896aa90a44
mohamed-elnagar0 <mohamed304.m@gmail.com>
2023-09-27 18:43:44 +0300

    add note state

--- lib/cubits/add_note_cubits/add_note_cubits_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'add_note_cubits_state.dart';
-
-class AddNoteCubitsCubit extends Cubit<AddNoteCubitsState> {
-  AddNoteCubitsCubit() : super(AddNoteCubitsInitial());
-}
--- lib/cubits/add_note_cubits/add_note_cubits_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'add_note_cubits_cubit.dart';
-
-@immutable
-sealed class AddNoteCubitsState {}
-
-final class AddNoteCubitsInitial extends AddNoteCubitsState {}
